0f085e40a3e4b08c15b2329b1749715cb901c314
darklefknight <darklefknight@web.de>
2017-10-24 22:41:57 +0200

    1. Increased Performance with cached function. 2. Prevent single characters to appear in top10 list.

--- Blatt_1/4-book-most-commomn.py
+++ Blatt_1/4-book-most-commomn.py
@@ -1,28 +1,42 @@
-import numpy as np
 import re
+from functools import lru_cache
+
+
+@lru_cache(maxsize=256)
+def removeStuffFromString(string):
+    string = string.replace("!", " ").replace("?", " ").replace(".", " ").replace(",", " ").replace(";", " ").replace(
+        "-", " ").replace("[", " ").replace("]", " ").replace("*", " ").replace("(", " ").replace(")", " ").replace(":",
+        " ").replace("'", " ")
+    return string
+
 
 if __name__ == "__main__":
     # FILE_PATH = "/home/bigdata/1/moby-dick.txt"
-    FILE_PATH = "C:/Users/darkl/Downloads/SampleTextFile_20kb.txt"
+    FILE_PATH = "C:/Users/darkl/Dropbox/moby-dick.txt"
 
     words = []
 
     with open(FILE_PATH, "r") as f:
-        lines = f.readlines()   # read whole file
+        lines = f.readlines()  # read whole file
 
-    lines = [line.strip() for line in lines]    # remove EOL-charachter from each line
-    lines = " ".join(lines).lower() # Join list into one string and make everything lower case
-
-    words = [] # initialize empty list
-    counter = {} # initialize empty dictionary
-    for word in lines.split():
-        word = word.replace(",","").replace(".","").replace("?","").replace("!","") # replace delimiters.
-        if not word in words:   # count every word just once
-            wordCount = len(re.findall(word,lines)) # count the word in the string
-            counter[word] = wordCount # append word to dictionary, with word as key and count as value
+    lines = [line.strip() for line in lines]  # remove EOL-charachter from each line
+    lines = " ".join(lines).lower()  # Join list into one string and make everything lower case
+    lines = removeStuffFromString(lines)
+    words = []  # initialize empty list
+    counter = {}  # initialize empty dictionary
+    for word in lines.split(" "):
+        word = removeStuffFromString(word)
+        if not word in words:  # count every word just once
+            if ((len(word) <= 1) and (word != "a") and (word != "i")): continue
+            wordCount = len(re.findall(word, lines))  # count the word in the string
+            if len(counter) < 11:
+                counter[word] = wordCount  # append word to dictionary, with word as key and count as value
+            elif min(sorted(counter.values(), reverse=True)[:-10]) \
+                < wordCount:  # just append if wordCount is bigger then smallest value in counter
+                counter[word] = wordCount  # append word to dictionary, with word as key and count as value
             words.append(word)
-
-    Top10 = sorted(counter, key=counter.get, reverse=True)[:10]     # get dictionary keys with 10 highest values
+    #
+    Top10 = sorted(counter, key=counter.get, reverse=True)[:10]  # get dictionary keys with 10 highest values
 
     for key in Top10:
-        print("%s, %i" %(key, counter[key]))    # print top 10 key plus value
+        print("%s, %i" % (key, counter[key]))  # print top 10 key plus value
