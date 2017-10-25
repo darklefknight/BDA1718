d70b241940ba2096e66e61667f2ee2f569556ba6
fiburg <finn.burgemeister@gmail.com>
2017-10-25 23:44:56 +0200

    initial version, in process thursday night :(

---
+++ Blatt_1/3-csv.R
@@ -0,0 +1,7 @@
+# The commandline arguments are available in commandArgs
+args = commandArgs(trailingOnly = TRUE)
+
+# Implement this function, do not use a function to completely parse the CSV file!
+computeMeanCSV = function(dateiname, spaltenNR){
+...
+}
