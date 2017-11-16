ac4fc9ee5be04c73aa4b18223a5c3c096ae958b6
Tobias Machnitzki <tobias.machnitzki@mpimet.mpg.de>
2017-11-16 16:26:34 +0100

    3 finished. Working at least on local machine with cat

--- Blatt_4/3-mapper.py
+++ Blatt_4/3-mapper.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python3
 import sys
 
+def mapMe(line):
+    splitted = line.split(",")
+    apps = ",".join(splitted[:4])
+    counts = ",".join(splitted[4:])
+    return_string = apps + "/" + counts
+    print(return_string)
+
+
 def main():
     for line in sys.stdin:
-        splitted = line.split(",")
-        apps = ",".join(splitted[:4])
-        counts = ",".join(splitted[4:])
-        return_string = apps + "/" + counts
-        print(return_string)
+        mapMe(line)
+
+
+def test():
+    lines = []
+    with open("/home/mpim/m300517/Downloads/data-energy-efficiency.csv") as f:
+        for i in range(10000):
+            lines.append(f.readline())
+
+    for line in lines:
+        mapMe(line)
+
 
 if __name__ == "__main__":
     main()
--- Blatt_4/3-reducer.py
+++ Blatt_4/3-reducer.py
@@ -1,6 +1,43 @@
-#!/usr/bin/python3
+#!/sw/jessie-x64/anaconda3-4.1.1/bin/python3
+
+import sys
+import numpy as np
 
 def main():
+    means = []
+    line_counter = 0
+    last_apps = " "
+    for line in sys.stdin:
+        try:
+            apps_1,count_str = line.split("/")
+            counts = count_str.split(",")
+            counts = [float(x) for x in counts]
+        except:
+            # print("error")
+            # print(line)
+            continue
+        apps = apps_1
+        if apps == last_apps:
+            line_counter += 1
+            means = np.divide(np.add(np.multiply(means,line_counter-1),counts),line_counter)
+            last_apps = apps
+
+        else:
+            means = [str(x) for x in means]
+            means = ",".join(means)
+            writeToFile(apps, means)
+            means = counts
+            line_counter = 1
+            last_apps = apps
+
+
+def writeToFile(app_str,mean_str):
+    write_path = ""
+    write_file = "energy-efficiency-counts.csv"
+    print(app_str)
+    with open(write_path + write_file, "a") as f:
+        f.write(app_str + " " + mean_str + "\n")
+
 
 
 if __name__ == "__main__":
