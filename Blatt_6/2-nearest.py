ff34df5d34116525432e4127c004a91ea93594ed
darklefknight <darklefknight@web.de>
2017-11-26 14:21:25 +0100

    try fetching Titles3

--- Blatt_6/2-nearest.py
+++ Blatt_6/2-nearest.py
@@ -6,7 +6,7 @@ def getPSQLatLoc(cur,lat,lon,distance):
     lat_high = lat + distance
     lon_low = lon - distance
     lon_high = lon + distance
-    cur.execute('SELECT "Titel" FROM wp_coords_red0 WHERE lat BETWEEN {0} AND {1} AND lon BETWEEN {2} AND {3}'.format(lat_low,lat_high,lon_low,lon_high))
+    cur.execute('SELECT lat,lon,"Titel" FROM wp_coords_red0 WHERE lat BETWEEN {0} AND {1} AND lon BETWEEN {2} AND {3}'.format(lat_low,lat_high,lon_low,lon_high))
     loc_list = cur.fetchall()
     return loc_list
 
@@ -17,5 +17,4 @@ if __name__ == "__main__":
 
     cur.close()
     conn.close()
-    for element in citty_list:
-        print(element)
+    print(citty_list)
