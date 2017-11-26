84d0ae67959d29fb41b8de5caac01a8a0f5013f2
darklefknight <darklefknight@web.de>
2017-11-26 14:31:45 +0100

    try fetching Titles with their coords1

--- Blatt_6/2-nearest.py
+++ Blatt_6/2-nearest.py
@@ -1,19 +1,24 @@
 import psycopg2
 
 
-def getPSQLatLoc(cur,lat,lon,distance):
-    lat_low = lat - distance
-    lat_high = lat + distance
-    lon_low = lon - distance
-    lon_high = lon + distance
-    cur.execute('SELECT lat,lon,"Titel" FROM wp_coords_red0 WHERE lat BETWEEN {0} AND {1} AND lon BETWEEN {2} AND {3}'.format(lat_low,lat_high,lon_low,lon_high))
-    loc_list = cur.fetchall()
+def getPSQLatLoc(cur,lat,lon,distance,closest_n):
+    while len(loc_list) < closest_n:
+        lat_low = lat - distance
+        lat_high = lat + distance
+        lon_low = lon - distance
+        lon_high = lon + distance
+
+        cur.execute('SELECT lat,lon,"Titel" FROM wp_coords_red0 WHERE lat BETWEEN {0} AND {1} AND lon BETWEEN {2} AND {3}'.format(lat_low,lat_high,lon_low,lon_high))
+        loc_list = cur.fetchall()
+        distance +=0.01
+
+
     return loc_list
 
 if __name__ == "__main__":
     conn = psycopg2.connect("dbname=postgis")
     cur = conn.cursor()
-    citty_list = getPSQLatLoc(cur,52.1,10,0.1)
+    citty_list = getPSQLatLoc(cur,52.1,10,0.01,10)
 
     cur.close()
     conn.close()
