839f35ae403244b59a83cd95a183c8c6c1ab2711
darklefknight <darklefknight@web.de>
2017-11-26 14:47:45 +0100

    try fetching Titles with their coords2

--- Blatt_6/2-nearest.py
+++ Blatt_6/2-nearest.py
@@ -1,5 +1,22 @@
 import psycopg2
+import numpy as np
 
+
+def distance(lat1,lon1,lat2,lon2):
+    """
+    Calculating the closest distance between two coordinates using the harvesine formula
+    """
+    R = 6371e3 # Radiuas of earth in m
+    phi1 = np.deg2rad(lat1)
+    phi2 = np.deg2rad(lat2)
+
+    delta_phi = np.deg2rad(lat2-lat1)
+    delta_lambda = np.deg2rad(lon2-lon1)
+
+    a = (np.sin(delta_phi/2)**2) + (np.cos(phi1) * np.cos(phi2) * (np.sin(delta_lambda/2)**2))
+    c = 2 * np.arctan2(np.sqrt(a),np.sqrt(1-a))
+    d = R * c
+    return d
 
 def getPSQLatLoc(cur,lat,lon,distance,closest_n):
     loc_list = []
@@ -19,9 +36,11 @@ def getPSQLatLoc(cur,lat,lon,distance,closest_n):
 if __name__ == "__main__":
     conn = psycopg2.connect("dbname=postgis")
     cur = conn.cursor()
-    citty_list = getPSQLatLoc(cur,52.1,10,0.01,10)
+    position = (53.1,10)
+    citty_list = getPSQLatLoc(cur,position[0],position[1],0.01,10)
 
     cur.close()
     conn.close()
     for tuple in citty_list:
-        print("%7.4f | %7.4f | %s"%(tuple[0],tuple[1],tuple[2]) )
+        dist = distance(position[0],position[1],tuple[0],tuple[1])
+        print("%7.4f | %s"%(dist,tuple[2]) )
