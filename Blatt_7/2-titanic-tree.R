690404d78537c804b19e5880672a95453e6667dc
fiburg <finn.burgemeister@gmail.com>
2017-12-05 15:13:58 +0100

    initial

--- Blatt_7/2-titanic-tree.R
+++ Blatt_7/2-titanic-tree.R
@@ -1,3 +1,9 @@
 library(rpart)
 
-dTitanic = read.csv("../../WORK/Blatt7/titanic.csv", header=T, sep=",", quote="\"")
+dTitanic = read.csv("/home/finn/Schreibtisch/Studium/17_WiSe/BigDataAnalytics/WORK/Blatt7/titanic.csv", header=T, sep=",", quote="\"")
+
+colnames(dTitanic)[1] = "Num"
+
+# Create a decision tree 
+m = rpart(Survived ~ Class, data=dTitanic, method="class",
+          control = rpart.control(minsplit=5, cp = 0.05))
