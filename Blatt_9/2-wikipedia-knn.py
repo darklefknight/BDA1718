9c3c60bb092c8528528f7ea2f3eb22b87b02fcb5
fiburg <finn.burgemeister@gmail.com>
2017-12-26 19:54:04 +0100

    some ideas...

--- Blatt_9/2-wikipedia-knn.py
+++ Blatt_9/2-wikipedia-knn.py
@@ -1,17 +1,22 @@
 import re
-import numpy as np
-
-
+from nltk.stem.snowball import SnowballStemmer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from collections import Counter
 from sklearn.neighbors import NearestNeighbors
 
-def split_line(line):
-    results = {"ID": [],
-               "link": [],
-               "topics": [],
-               "title": []
-               }
+stemmer = SnowballStemmer("english") # defines the stemmer language
+
+
+def split_line(results, line):
+    """
+    :param results: result dict
+    :param line: wiki content line
+    :return results: structurized data with stemmed words with frequency
+    """
+
+    # Split:
     line = line.rstrip()
-    # print(line)
+
     # By splitting the line at the comma we can easy extract the ID, link and title:
     comma_split = line.split(",")
     results["ID"].append(int(comma_split[0]))
@@ -23,7 +28,43 @@ def split_line(line):
     topic_list = re.findall(r"'(.*?)'", klammer_split[-1])
     results["topics"].append(topic_list)
 
+    # Find text: (this is just everything in between the last two)
+    text = " ".join(comma_split[3:])
+    text = " ".join(text.split("[")[:-1])
+    text = text.replace('"', '')
+
+    text = cleanText(text)  # get rid of everything else than plain text
+
+    wordlist = text.split(" ")  # each word becomes one element in this list
+
+    # Stemming:
+    results["content"].append([stemWord(word) for word in wordlist])
+    #count_stemmed = countWords(stemmed_list)
+    #del count_stemmed["s"]
+    #results["content"].append(count_stemmed)
+
     return results
+
+
+def cleanText(text: str):
+    """
+    :param text: just any string
+    :return: cleaned string just containing charachters a-z and A-Z, nothing else
+    """
+    return re.sub('[^A-Za-z]+', ' ', text)
+
+
+def stemWord(word: str):
+    """
+    :param word: a single word as string
+    :return: the stemm of that word as string
+    """
+    stemmed = stemmer.stem(word)
+    if (len(stemmed) > 2) or (stemmed == "a") or (stemmed == "i"):
+        return stemmed.lower()
+    else: # something needs to be returned (this is due to how this function is called)
+        return 's' # since s is not a valid return it is easy to throw it out later
+
 
 def get_lines_of_file(file):
     with open(file,"r") as f:
@@ -43,6 +84,23 @@ if __name__ == "__main__":
     test = sep*2
     validation = lines
 
+    wiki_struc = {"ID": [],
+               "link": [],
+               "topics": [],
+               "title": [],
+               "content": []
+    }
+
+    with open(FILE, "r") as f:
+        for i, line in enumerate(f):
+            if i > training:
+                break
+            wiki_struc = split_line(wiki_struc, line)
+
+    print(len(wiki_struc["content"]))
+
+    """
+    # IDEA
     # ========================
     # TRAINING:
     # ========================
@@ -65,7 +123,7 @@ if __name__ == "__main__":
                     inter_set = set(results["topics"][0]).intersection(results2["topics"][0])
                     if len(inter_set) >=2:
                         print(inter_set,i,j)
-
+    """
 
 
 
