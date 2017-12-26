23f6cd6485dbf45334f90dbda7ada5e7cdff41e9
fiburg <finn.burgemeister@gmail.com>
2017-12-26 21:53:25 +0100

    final version (testing not possible because of server problems)

---
+++ Blatt_9/1-hbase-query.py
@@ -0,0 +1,30 @@
+#!/bin/user/python3
+
+import happybase
+
+def connect_to_hbase(host, table_name):
+    """Connects to HBase
+    :param host:
+    :param table_name:
+    :return:
+    """
+    connection = happybase.Connection(host=host)
+    connection.open()
+
+    table = connection.table(table_name)
+
+    return connection, table
+
+
+def check_for_word(word, table)
+    article_id = []
+    for key, data in table.scan(columns="data:text"):
+        if word.lower() in data.lower():
+            article_id.append(key)
+    return article_id
+
+
+if __name__ == "__main__":
+    table_name = "machnitzki_burgemeister"
+    conn, table = connect_to_hbase("abu2", table_name)
+    articles_affected = check_for_word("test", table)
