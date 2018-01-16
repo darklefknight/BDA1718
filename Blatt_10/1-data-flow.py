61b42d5199e2a34b12336e5713fb06387314e8b9
Tobias Machnitzki <tobias.machnitzki@mpimet.mpg.de>
2018-01-16 14:32:31 +0100

    Skeleton built. (initial commit)

---
+++ Blatt_10/1-data-flow.py
@@ -0,0 +1,32 @@
+class DataFlow(object):
+
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return "Nothing done here yet!"
+
+    def read(self,file):
+        pass
+
+    def write(self,file):
+        pass
+
+    def filter(self,function):
+        pass
+
+    def map(self,function):
+        pass
+
+    def flatmap(self,function):
+        pass
+
+    def group(self,function):
+        pass
+
+    def reduce(self,function):
+        pass
+
+    def join(self,function):
+        pass
+
