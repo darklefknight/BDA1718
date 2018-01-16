6690981f972eee874937dd7ff1dc8ab93d5c70d8
Tobias Machnitzki <tobias.machnitzki@mpimet.mpg.de>
2018-01-16 17:35:12 +0100

    Group implemented successfully. Write function adjusted to new dictionary format. __str__ adjusted as well.

--- Blatt_10/1-data-flow.py
+++ Blatt_10/1-data-flow.py
@@ -11,6 +11,11 @@ class DataFlow(object):
 
         :return:
         """
+        if type(self.data) == dict:
+            s1 = "The data has been Grouped. The Following keys are valid:\n%s\n" % ", ".join(self.data.keys())
+            s2 = "Depending of your input data your keys might be strings!"
+            return s1
+
         if self.data != None:
             s1 = "The Data consists: \n%4i Rows\n%4i Columns\n\n"%(len(self.data),len(self.data[0]))
             s2 = "The shape of the Columns is:\n%s"%",".join([" " +str(_) + " " + str(type(i)) for _,i in enumerate(self.data[0])])
@@ -18,6 +23,14 @@ class DataFlow(object):
 
         else:
             return "Class to process data from csv files."
+
+    def copy(self):
+        """
+        After the idea from numpy-arrays.
+
+        :return: new instance of DataFlow with the same data.
+        """
+        return self.__returnDataFlow(data=self.data)
 
     def read(self,file,delimiter=",",comments="#",header_lines=0):
         """
@@ -57,11 +70,36 @@ class DataFlow(object):
     def write(self,file):
         """
         Writes the content which currently is stored in self.data into a given file.
+        Dependent on if the data was grouped (is now in dictionary format) the output will be different.
+        If in dictionary form output will be in the format:
+
+            key1;[(v1,v2),(v3,v4),(....)]
+            key2;[(v1,v2),(v3,v4),(....)]
+            ...
+
+        Else the output will be in the format:
+
+            v1;v2;v3;v4
+            v5;v6;v7;v8
+            ...
+
+
         :param file: String of the file name to store the data into.
         """
-        with open(file,"w") as f:
-            for line in self.data:
-                f.write("%s\n"%";".join([str(s) for s in line])) # converts each element of each tuple into a string
+
+        if not type(self.data) == dict:
+            with open(file,"w") as f:
+                for line in self.data:
+                    f.write("%s\n"%";".join([str(s) for s in line])) # converts each element of each tuple into a string
+
+        else:
+            with open(file,"w") as f:
+                for key in self.data:
+                    f.write(str(key))
+                    f.write(";")
+                    f.write(str(self.data[key]))
+                    f.write("\n")
+
 
     def filter(self,function):
         """
@@ -81,7 +119,6 @@ class DataFlow(object):
             if function(line) == True:
                 self.new_data.append(line)
 
-        self.data = self.new_data.copy() # the copy() is to not just point to the same location in the storage.
 
         return self.__returnDataFlow()
 
@@ -99,15 +136,62 @@ class DataFlow(object):
         for line in self.data:
             self.new_data.append(function(line)) # append the result of the function for each line.
 
-        self.data = self.new_data.copy() # the copy() is to not just point to the same location in the storage.
-
         return self.__returnDataFlow()
 
     def flatmap(self,function):
+        """
+        I did not really understand what this should do.
+        """
         pass
 
+
     def group(self,function):
-        pass
+        """
+        Groups the data according to the input function. The output is a dictionary.
+
+        :param function:
+        :return:
+        """
+        self.new_data = {}
+        key_list = []
+        for line in self.data:
+            key = function(line)
+            if not key in key_list:
+                result_line = []
+                if type(key) in (list,tuple):
+                    for element in line:
+                        if not element in key:
+                            result_line.append(element)
+
+                    result = tuple(result_line)
+                    self.new_data[key] = [result]
+
+                else:
+                    for element in line:
+                        if not element == key:
+                            result_line.append(element)
+                    result =  tuple(result_line)
+                    self.new_data[key] = [result]
+
+                key_list.append(key)
+            else:
+                result_line = []
+                if type(key) in (list, tuple):
+                    for element in line:
+                        if not element in key:
+                            result_line.append(element)
+
+                    result = tuple(result_line)
+                    self.new_data[key].append(result)
+
+                else:
+                    for element in line:
+                        if not element == key:
+                            result_line.append(element)
+                    result = tuple(result_line)
+                    self.new_data[key].append(result)
+
+        return self.__returnDataFlow()
 
     def reduce(self,function):
         pass
@@ -115,15 +199,25 @@ class DataFlow(object):
     def join(self,function):
         pass
 
-    def __returnDataFlow(self):
-        return DataFlow(self.new_data)
+    def __returnDataFlow(self,data=None):
+        """
+        Creates a new instance with the processed data, so that the data of the original instance is not touched.
+
+        :return: DataFlow-object
+        """
+        if not data:
+            return DataFlow(self.new_data)
+        else:
+            return DataFlow(data)
 
 
 
 if __name__ == "__main__":
     FILE = "/home/mpim/m300517/Downloads/Cart_Site201602.csv"
-    d = DataFlow()
-    a = d.read(FILE,delimiter=";",header_lines=2)
+    df = DataFlow()
+    data = df.read(FILE,delimiter=";",header_lines=2)
 
-
+    filtered = data.filter(lambda x : x[-1] < 1) # keep lines where laast column is smaller than 1
+    mapped = data.map(lambda x : (x[0],x[1],x[-2])) # map first, second and second-last column
+    grouped = data.group(lambda x : x[0]) # group by first column
 
