63495ae8d9b35a74e8ebb030c5ebf25c72762de8
Tobias Machnitzki <tobias.machnitzki@mpimet.mpg.de>
2018-01-24 14:14:26 +0100

    Try to split the words by article

--- Blatt_11/2-wikipedia-distanz.py
+++ Blatt_11/2-wikipedia-distanz.py
@@ -5,17 +5,24 @@ import collections
 # Call spark using this for example:
 # PYSPARK_PYTHON=python3 pyspark --master yarn-client --driver-memory 500m --executor-memory 500m --conf spark.ui.port=4711
 
-conf = SparkConf()
-conf.setAppName("MyApp")
-sc = SparkContext(conf=conf)
 
-def parseInput(file_name="/user/bigdata/enwiki-10k.csv"):
+
+def parseInput(sc,file_name="/user/bigdata/enwiki-10k.csv"):
     # Read in the data and clean it
     rdd = sc.textFile(file_name)
     return rdd
 
 
 
-rdd = parseInput()
-# Print the first 10 lines.
-print(rdd.take(10))
+if __name__ == "__main__":
+    conf = SparkConf()
+    conf.setAppName("Distance")
+    sc = SparkContext(conf=conf)
+    articles = parseInput(sc)
+
+    # counter = sc.accumulator(0)
+    # numerated_articles = articles.map(lambda x: (x,counter.add(1)))
+    words_in_article = articles.map(lambda x: (x.split(" ")))
+
+
+
