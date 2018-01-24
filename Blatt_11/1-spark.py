476fea51d4f4bd25ca99f8dcdd9a239dfd507638
fiburg <finn.burgemeister@gmail.com>
2018-01-24 13:00:43 +0100

    pre final

--- Blatt_11/1-spark.py
+++ Blatt_11/1-spark.py
@@ -1,4 +1,23 @@
-# operations sparks local mode
+# Create an RDD using /user/bigdata/enwiki-10k.csv
+lines = sc.textFile("/user/bigdata/enwiki-10k.csv")
 
-rdd_read = sc.textFile("/user/bigdata/enwiki-10k.csv")
+# RDD with articles which contain the word computer
+cmpcont = lines.filter(lambda x: "computer" in x.lower())
+
+# compute the number of articles (absolute and relative to the total number)
+cmpcont.count()                     # absolute (230)
+cmpcont.count() / lines.count()     # relative
+
+# Save the articles that contain the word into a file on HDFS
+cmpcont.saveAsTextFile("/user/burgemeister/1-spark-computer.txt")
+
+# Compute the number of articles containing the word computer again using only accumulators
+counter = sc.accumulator(0)
+cmpcont.foreach(lambda x: counter.add(1))
+counter.value
+
+# Now use the methods for data frames for computing the number of articles containing the word computer
+cmpcont_df = cmpcont.map(lambda x: (x, )).toDF()
+cmpcont_df.count()
+
 
